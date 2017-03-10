8ef77b4ccc6c7338bb81517ee5f60f22dd865a6e
zhangleigang <zhangleigang@jd.com>
2017-03-10 21:35:07 +0800

    代码精简

--- src/docpage/docpage.js
+++ src/docpage/docpage.js
@@ -2,26 +2,26 @@
  * Created by jf on 2015/9/11.
  * Modified by bear on 2016/9/7.
  */
-$(function () {
+$(function() {
     var pageManager = {
         $container: $('#container'),
         _pageStack: [],
         _configs: [],
-        _pageAppend: function(){},
+        _pageAppend: function() {},
         _defaultPage: null,
         _pageIndex: 1,
-        setDefault: function (defaultPage) {
+        setDefault: function(defaultPage) {
             this._defaultPage = this._find('name', defaultPage);
             return this;
         },
-        setPageAppend: function (pageAppend) {
+        setPageAppend: function(pageAppend) {
             this._pageAppend = pageAppend;
             return this;
         },
-        init: function () {
+        init: function() {
             var self = this;
 
-            $(window).on('hashchange', function () {
+            $(window).on('hashchange', function() {
                 var state = history.state || {};
                 var url = location.hash.indexOf('#') === 0 ? location.hash : '#';
                 var page = self._find('url', url) || self._defaultPage;
@@ -43,25 +43,27 @@ $(function () {
             this._go(page);
             return this;
         },
-        push: function (config) {
+        push: function(config) {
             this._configs.push(config);
             return this;
         },
-        go: function (to) {
+        go: function(to) {
             var config = this._find('name', to);
             if (!config) {
                 return;
             }
             location.hash = config.url;
         },
-        _go: function (config) {
-            this._pageIndex ++;
+        _go: function(config) {
+            this._pageIndex++;
 
-            history.replaceState && history.replaceState({_pageIndex: this._pageIndex}, '', location.href);
+            history.replaceState && history.replaceState({
+                _pageIndex: this._pageIndex
+            }, '', location.href);
 
             var html = $(config.template).html();
             var $html = $(html).addClass('slideIn').addClass(config.name);
-            $html.on('animationend webkitAnimationEnd', function(){
+            $html.on('animationend webkitAnimationEnd', function() {
                 $html.removeClass('slideIn').addClass('js_show');
             });
             this.$container.append($html);
@@ -75,13 +77,15 @@ $(function () {
                 this._bind(config);
             }
 
+            Prism.highlightAll();
+
             return this;
         },
-        back: function () {
+        back: function() {
             history.back();
         },
-        _back: function (config) {
-            this._pageIndex --;
+        _back: function(config) {
+            this._pageIndex--;
 
             var stack = this._pageStack.pop();
             if (!stack) {
@@ -105,15 +109,15 @@ $(function () {
                 });
             }
 
-            stack.dom.addClass('slideOut').on('animationend webkitAnimationEnd', function () {
+            stack.dom.addClass('slideOut').on('animationend webkitAnimationEnd', function() {
                 stack.dom.remove();
             });
 
             return this;
         },
-        _findInStack: function (url) {
+        _findInStack: function(url) {
             var found = null;
-            for(var i = 0, len = this._pageStack.length; i < len; i++){
+            for (var i = 0, len = this._pageStack.length; i < len; i++) {
                 var stack = this._pageStack[i];
                 if (stack.config.url === url) {
                     found = stack;
@@ -122,7 +126,7 @@ $(function () {
             }
             return found;
         },
-        _find: function (key, value) {
+        _find: function(key, value) {
             var page = null;
             for (var i = 0, len = this._configs.length; i < len; i++) {
                 if (this._configs[i][key] === value) {
@@ -132,7 +136,7 @@ $(function () {
             }
             return page;
         },
-        _bind: function (page) {
+        _bind: function(page) {
             var events = page.events || {};
             for (var t in events) {
                 for (var type in events[t]) {
@@ -143,37 +147,8 @@ $(function () {
         }
     };
 
-    function fastClick(){
-        var supportTouch = function(){
-            try {
-                document.createEvent("TouchEvent");
-                return true;
-            } catch (e) {
-                return false;
-            }
-        }();
-        var _old$On = $.fn.on;
-
-        $.fn.on = function(){
-            if(/click/.test(arguments[0]) && typeof arguments[1] == 'function' && supportTouch){ // 只扩展支持touch的当前元素的click事件
-                var touchStartY, callback = arguments[1];
-                _old$On.apply(this, ['touchstart', function(e){
-                    touchStartY = e.changedTouches[0].clientY;
-                }]);
-                _old$On.apply(this, ['touchend', function(e){
-                    if (Math.abs(e.changedTouches[0].clientY - touchStartY) > 10) return;
-
-                    e.preventDefault();
-                    callback.apply(this, [e]);
-                }]);
-            }else{
-                _old$On.apply(this, arguments);
-            }
-            return this;
-        };
-    }
-    function preload(){
-        $(window).on("load", function(){
+    function preload() {
+        $(window).on("load", function() {
             var imgList = [
                 "./images/layers/content.png",
                 "./images/layers/navigation.png",
@@ -185,30 +160,16 @@ $(function () {
             }
         });
     }
-    function androidInputBugFix(){
-        // .container 设置了 overflow 属性, 导致 Android 手机下输入框获取焦点时, 输入法挡住输入框的 bug
-        // 相关 issue: https://github.com/weui/weui/issues/15
-        // 解决方法:
-        // 0. .container 去掉 overflow 属性, 但此 demo 下会引发别的问题
-        // 1. 参考 http://stackoverflow.com/questions/23757345/android-does-not-correctly-scroll-on-input-focus-if-not-body-element
-        //    Android 手机下, input 或 textarea 元素聚焦时, 主动滚一把
-        if (/Android/gi.test(navigator.userAgent)) {
-            window.addEventListener('resize', function () {
-                if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
-                    window.setTimeout(function () {
-                        document.activeElement.scrollIntoViewIfNeeded();
-                    }, 0);
-                }
-            })
-        }
-    }
 
-    function setPageManager(){
-        var pages = {}, tpls = $('script[type="text/html"]');
+    function setPageManager() {
+        var pages = {},
+            tpls = $('script[type="text/html"]');
         var winH = $(window).height();
 
         for (var i = 0, len = tpls.length; i < len; ++i) {
-            var tpl = tpls[i], name = tpl.id.replace(/tpl_/, '');
+            var tpl = tpls[i],
+                name = tpl.id.replace(/tpl_/, '');
+
             pages[name] = {
                 name: name,
                 url: '#' + name,
@@ -220,29 +181,15 @@ $(function () {
         for (var page in pages) {
             pageManager.push(pages[page]);
         }
-        pageManager
-            .setPageAppend(function($html){
-                var $foot = $html.find('.page__ft');
-                if($foot.length < 1) return;
-
-                if($foot.position().top + $foot.height() < winH){
-                    $foot.addClass('j_bottom');
-                }else{
-                    $foot.removeClass('j_bottom');
-                }
-            })
-            .setDefault('home')
-            .init();
+        pageManager.setDefault('home').init();
     }
 
-    function init(){
+    function init() {
         preload();
-        fastClick();
-        androidInputBugFix();
         setPageManager();
 
         window.pageManager = pageManager;
-        window.home = function(){
+        window.home = function() {
             location.hash = '';
         };
     }
